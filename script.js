55191bf9b463da874fc0336dbe8a8e348268044d
Balaj-Maqbool <balajmaqbool54@gmail.com>
2024-07-25 01:09:08 -0700

    adding JS

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const showSignUpButton = document.getElementById('show-sign-up');
-    const showSignInButton = document.getElementById('show-sign-in');
-    const signUpSlider = document.querySelector('.sign-up-slider');
-    const signInSlider = document.querySelector('.sign-in-slider');
+let signUpSlider = document.querySelector(".sign-up-slider");
+let logInSlider = document.querySelector(".sign-in-slider");
+let signupBtn = document.querySelector("#sign-up-button");
+let loginBtn = document.querySelector("#sign-in-button");
+let signupForm = document.querySelector(".sign-up-form");
+let loginForm = document.querySelector(".log-in-form");
 
-    showSignUpButton.addEventListener('click', function() {
-        signUpSlider.classList.add('active');
-        signInSlider.classList.remove('active');
-    });
+signupBtn.onclick = () => {
+  signUpSlider.classList.add("SignUpSlideRight");
+  logInSlider.classList.add("SignInSlideRight");
+  signupForm.classList.add("signUpFormVisibilityRight");
+  loginForm.classList.add("signInFormVisibilityRight");
 
-    showSignInButton.addEventListener('click', function() {
-        signInSlider.classList.add('active');
-        signUpSlider.classList.remove('active');
-    });
-});
+  signUpSlider.classList.remove("SignUpSlideLeft");
+  logInSlider.classList.remove("SignInSlideLeft");
+  signupForm.classList.remove("signUpFormVisibilityLeft");
+  loginForm.classList.remove("signInFormVisibilityLeft");
+};
+
+loginBtn.onclick = () => {
+  signUpSlider.classList.remove("SignUpSlideRight");
+  logInSlider.classList.remove("SignInSlideRight");
+  signupForm.classList.remove("signUpFormVisibilityRight");
+  loginForm.classList.remove("signInFormVisibilityRight");
+
+  signUpSlider.classList.add("SignUpSlideLeft");
+  logInSlider.classList.add("SignInSlideLeft");
+  signupForm.classList.add("signUpFormVisibilityLeft");
+  loginForm.classList.add("signInFormVisibilityLeft");
+};
